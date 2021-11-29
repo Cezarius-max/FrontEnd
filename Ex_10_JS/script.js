528feb0648997fdbb2998530bbeb14743a513a63
Cezarius-max <cezary150@gmail.com>
2021-11-29 10:33:50 +0100

    JS

---
+++ Ex_10_JS/script.js
@@ -0,0 +1,25 @@
+// // console.log("Hello from Script.js");
+
+// const stala = 29;
+// let zmienna = 29;
+
+// // Constów nie da sie nadpisać, a Let-y się da
+// const stala = 10; 
+// let zmienna = 10;
+
+// console.log("stala: " + stala);
+// console.log("zmienna: "+zmienna);
+const text = "Mogę Tu coś wpisać";
+
+const object = {
+    name:"john",
+    age: 29,
+    dog: {
+        name: "Azor",
+        race: "Labrador"
+    }
+};
+
+console.log(object);
+console.log(object.name)
+console.log(object.dog.name)
